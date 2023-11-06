358fe745bf9561c9d8723b20037759a5320307c0
cosimogiove <cosimo18497@gmail.com>
2023-11-06 17:06:42 +0100

    feat: fine esercizio

---
+++ main.js
@@ -0,0 +1,39 @@
+" use strect"
+//chiedere all'utente il numero di km da percorrere e salvarlo in una variabile prompt//
+
+let NumeroKm = prompt("Numeri di km da percorrere")
+NumeroKm = parseInt(NumeroKm)
+console.log("Numero di km:", NumeroKm)
+
+//chiedere all'utente l'età del passegero e salvarlo in una variabile prompt//
+
+const EtaPasseggero = prompt("Età del passeggero")
+console.log("età del passeggero:",EtaPasseggero)
+
+//salvare in una variabile il prezzo al km//
+let PrezzoKm= NumeroKm*0.21;
+PrezzoKm.toFixed(2)
+
+console.log( "Costo totale = ",PrezzoKm)
+//calcolare il prezzo del biglietto in base ai km percorsi dall'utente (prezzo al km 0.21 euro)//
+
+/*Condizioni
+se l'utente ha un età compresa dai 0 a 17 anni:*/
+
+const sconto2= PrezzoKm*0.4;
+const sconto =PrezzoKm*0.2;
+//sconto del 20%//
+if (EtaPasseggero < 18){
+    PrezzoKm= PrezzoKm - sconto
+    console.log("Prezzo scontato totale da pagare : ", PrezzoKm.toFixed(2))
+
+//sconto del 40%//
+}else if (EtaPasseggero > 65){
+
+    PrezzoKm= PrezzoKm - sconto2
+    console.log("prezzo scontato totale da pagare:", PrezzoKm.toFixed(2))
+}
+ else{
+    console.log("niente sconto")
+ }
+ 
